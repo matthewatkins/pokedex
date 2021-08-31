e24dffdd18a30645402137007209149fa62dfe0b
Matthew Atkins <matthew@atkins.codes>
2021-08-31 15:02:21 -0500

    testing static loading all pokemon

--- pages/index.js
+++ pages/index.js
@@ -12,53 +12,53 @@ export default function Home({pokemons}) {
   const [search, setSearch] = useState('');
 
   // lazy load
-  const getMorePokemon = async () => {
-    if(allPokemon.length >= 898) {
-      setMorePokemon(false);
-      setAllPokemon(allPokemon.slice(0, 898));
-      return;
-    }
+  // const getMorePokemon = async () => {
+  //   if(allPokemon.length >= 898) {
+  //     setMorePokemon(false);
+  //     setAllPokemon(allPokemon.slice(0, 898));
+  //     return;
+  //   }
 
-    const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=100&offset=${allPokemon.length}`);
-    const newPokemon = await res.json();
+  //   const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=100&offset=${allPokemon.length}`);
+  //   const newPokemon = await res.json();
 
-    setAllPokemon((pokemon) => [...pokemon, ...newPokemon.results]);
-  };
+  //   setAllPokemon((pokemon) => [...pokemon, ...newPokemon.results]);
+  // };
 
   // search
   // const [searchAPI, setSearchAPI] = useState(false);
   const [searchedPokemon, setSearchedPokemon] = useState([]);
-  console.log(searchedPokemon);
+  // console.log(searchedPokemon);
 
-  const searchForPokemon = debounce(async (search) => {
-    if(search.length) {
-      const res = await fetch(`http://localhost:3000/api/search?name=${search}`);
-      try {
-        const pokemon = await res.json();
-        setSearchedPokemon(pokemon);
-      } catch (error) {
-        console.log('error searching pokemon');
-      }
-    } else {
-      setSearchedPokemon([]);
-    }
-  }, 1000);
+  // const searchForPokemon = debounce(async (search) => {
+  //   if(search.length) {
+  //     const res = await fetch(`http://localhost:3000/api/search?name=${search}`);
+  //     try {
+  //       const pokemon = await res.json();
+  //       setSearchedPokemon(pokemon);
+  //     } catch (error) {
+  //       console.log('error searching pokemon');
+  //     }
+  //   } else {
+  //     setSearchedPokemon([]);
+  //   }
+  // }, 1000);
 
   // adding a function to check if searched pokemon is already loaded
   // if it is, then don't fetch it again
-  const pokemonCurrentlyExists = (query) => {
-    return allPokemon.some(function (pokemon) {
-      let pokemonExists = pokemon.name === query;
+  // const pokemonCurrentlyExists = (query) => {
+  //   return allPokemon.some(function (pokemon) {
+  //     let pokemonExists = pokemon.name === query;
 
-      return pokemonExists;
-    });
-  }
+  //     return pokemonExists;
+  //   });
+  // }
 
   const handleSearch = useCallback((event) => {
     setSearch(event.target.value);
-    if(!pokemonCurrentlyExists(event.target.value)) {
-      searchForPokemon(event.target.value);
-    }
+    // if(!pokemonCurrentlyExists(event.target.value)) {
+    //   searchForPokemon(event.target.value);
+    // }
   }, []);
 
   useEffect(() => {
@@ -81,26 +81,26 @@ export default function Home({pokemons}) {
           <input type="search" name="search" id="search" className={styles.search} onChange={handleSearch} value={search} placeholder="Search for a pokemon" autoComplete="off" />
         </p>
 
-        <div>
-        <InfiniteScroll
+        <div className={styles.grid}>
+        {/* <InfiniteScroll
           dataLength={allPokemon.length}
           next={getMorePokemon}
           hasMore={morePokemon}
           loader={<PokeSpinner />}
           endMessage={<span className={styles.endMessage}>No more Pokemon!</span>}
           className={styles.grid}
-        >
+        > */}
             {allPokemon
               .filter(pokemon => pokemon.name?.toLowerCase().includes(search.toLowerCase()))
               .map((pokemon, index) => (
                 <Link href="/[pokemon]" as={`/${pokemon.name}`} key={index}>
                   <a className={styles.card}>
-                    {index <= 896 && <img src={`https://ik.imagekit.io/n7nxxqwkxic/pokemons/tr:w-200/${pokemon.name}.png`} alt={pokemon.name} />}
+                    {index <= 897 && <img src={`https://ik.imagekit.io/n7nxxqwkxic/pokemons/tr:w-200/${pokemon.name}.png`} alt={pokemon.name} />}
                     <p className={styles.title} style={{textTransform: 'capitalize'}}>{pokemon.name?.replace(/-/g, ' ')}</p>
                   </a>
                 </Link>
           ))}
-          </InfiniteScroll>
+          {/* </InfiniteScroll> */}
           </div>
       </main>
 
@@ -113,7 +113,7 @@ export default function Home({pokemons}) {
 }
 
 export async function getStaticProps() {
-  const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100'); // limit to 898 max
+  const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=898'); // limit to 898 max
   const { results } = await res.json();
 
   const pokemons = results.map((pokemon, index) => {
